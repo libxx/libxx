d21d07a058e7557fe4cedcfd9de68ad4266bfa9b
Libxx <me@libxx.com>
2016-09-12 18:43:37 +0800

    added cache dir to gitignore

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -3,16 +3,17 @@
 return [
 
     'debug' => false,
+    'config_cache_enabled' => true,
 
     'factories' => [
+        \Psr\Http\Message\ServerRequestInterface::class => \App\Factory\RequestFactory::class,
         \Libxx\Error\ExceptionHandlerInterface::class => \App\Factory\ExceptionHandlerFactory::class,
     ],
     'objects' => [
         \Libxx\Kernel\App::class,
-        \Psr\Http\Message\ServerRequestInterface::class => \Zend\Diactoros\ServerRequestFactory::fromGlobals(),
         \Psr\Http\Message\ResponseInterface::class => \Zend\Diactoros\Response::class,
         \Libxx\Error\ErrorHandlerInterface::class => \Libxx\Error\ErrorHandler::class,
-        \Zend\Diactoros\Response\EmitterInterface::class => new \Zend\Diactoros\Response\SapiEmitter(),
+        \Zend\Diactoros\Response\EmitterInterface::class => \Zend\Diactoros\Response\SapiEmitter::class,
         \Libxx\Middleware\DispatcherInterface::class => \Libxx\Middleware\PathBasedDispatcher::class,
         \Libxx\Kernel\CallableResolverInterface::class => \Libxx\Kernel\CallableResolver::class,
         \Libxx\Routing\RouterInterface::class => \Libxx\Routing\Router::class,
